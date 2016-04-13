865a711691ff986b5b1270f84f294ff02ee0f9f0
Gregory Estrade <gregory.estrade@gmail.com>
2016-04-13 15:15:07 +0200

    Added Create, Update and Delete

--- src/main/java/com/torlus/api/Application.java
+++ src/main/java/com/torlus/api/Application.java
@@ -2,6 +2,8 @@ package com.torlus.api;
 
 import static spark.Spark.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lyra.pnf.Toolbox;
+import com.lyra.pnf.entities.PNFMatch;
 import com.torlus.api.resources.*;
 
 public class Application {
@@ -24,12 +26,13 @@ public class Application {
 		
 		// port(4567);
 		staticFileLocation("front-end");
-				
+
 		options("/users", (req, res) -> {
 			Toolbox.options(res);
 			Toolbox.cors(res);
 			return "";
 		});
+		// READ
 		get("/users", (req, res) -> {
 			Toolbox.jsonOut(res);
 			Toolbox.cors(res);
@@ -45,6 +48,43 @@ public class Application {
 			} else {
 				return om.writeValueAsString(u);
 			}
+		});
+
+		// UPDATE
+		post("/matches/:id", (req, res) -> {
+			Toolbox.jsonOut(res);
+			Toolbox.cors(res);
+			String id = req.params(":id");
+			User u = users.find(id);
+			if (u == null) {
+				res.status(404);
+				return res.body();
+			}
+			try {
+				u = om.readValue(req.body(), User.class);
+				u = users.replace(id, u);
+			} catch(Exception ex) {
+				ex.printStackTrace();
+				res.status(500);
+				return "";
+			}
+			res.status(200);
+			return om.writeValueAsString(u);
+		});
+
+		// DELETE
+		post("/matches/:id/delete", (req, res) -> {
+			Toolbox.jsonOut(res);
+			Toolbox.cors(res);
+			String id = req.params(":id");
+			User u = users.find(id);
+			if (u == null) {
+				res.status(404);
+			} else {
+				users.delete(id);
+				res.status(204);
+			}
+			return "";
 		});
 
 	}
